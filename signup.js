d8642ec968dc246471b056754d399b9aee13f77c
samba4real <richhomiesamba4@gmail.com>
2022-05-10 08:35:31 +0000

    first commit

---
+++ signup.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.btn')
+
+
+btn.addEventListener('click', () => {
+    alert("Your account have been created successfully.")
+    alert('Do you to see the Product?')
+})
